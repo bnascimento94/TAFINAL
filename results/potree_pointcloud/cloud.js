246eea913d6f457945cdd200ab999000ffe7c00c
bnascimento94 <bnascimento94@gmail.com>
2019-05-04 18:56:11 +0100

    WebODM results

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 12661088,
+    "boundingBox": {
+        "lx": 554035.23,
+        "ly": 4505755.31,
+        "lz": -18.3,
+        "ux": 554444.81,
+        "uy": 4506164.89,
+        "uz": 391.2800000000745
+    },
+    "tightBoundingBox": {
+        "lx": 554035.23,
+        "ly": 4505755.3100000009,
+        "lz": -18.3,
+        "ux": 554444.81,
+        "uy": 4506088.8100000009,
+        "uz": 62.410000000000007
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.837653398513794,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
